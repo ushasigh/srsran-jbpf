39ade2ca1c41e279fbd2e7b9d5787011bc9793d6
Matthew Balkwill <mabalkwi@microsoft.com>
2025-03-04 16:42:32 +0000

    Update inline with new version of jrt-controller JrtcApp class

--- jrtc_apps/xran_packets/xran_packets.py
+++ jrtc_apps/xran_packets/xran_packets.py
@@ -9,17 +9,13 @@ from typing import Any
 from dataclasses import dataclass
 
 
-JRTC_PYTHON_INC = os.environ.get("JRTC_PYTHON_INC")
-if JRTC_PYTHON_INC is None:
-    raise ValueError("JRTC_PYTHON_INC not set")
-sys.path.append(f"{JRTC_PYTHON_INC}")
+JRTC_APP_PATH = os.environ.get("JRTC_APP_PATH")
+if JRTC_APP_PATH is None:
+    raise ValueError("JRTC_APP_PATH not set")
+sys.path.append(f"{JRTC_APP_PATH}")
 
 import jrtc_app
 from jrtc_app import *
-
-JRTC_PATH = f'{os.environ.get("JRTC_PATH")}'
-if JRTC_PATH is None:
-    raise ValueError("JRTC_PATH not set")
 
 
 JBPF_CODELETS = os.environ.get("JBPF_CODELETS")
@@ -34,70 +30,76 @@ from xran_packet_info import struct__packet_stats
 # state variables
 ########################################################
 @dataclass
-class AppStateVars:
-    wrapper: JrtcPythonApp
+class AppStateVars(ctypes.Structure):
+    _fields_ = [
+        ("app", ctypes.POINTER(JrtcApp)),
+    ]   
+
+
+########################################################
+# message handler
+########################################################
+def app_handler(timeout: bool, stream_idx: int, data_entry_ptr: ctypes.POINTER(struct_jrtc_router_data_entry), state_ptr: int):
+
+    XRAN_CODELET_OUT_SIDX = 0
+
+    if timeout:
+        ## timeout processing
+        return
+
+    # Dereference the pointer arguments
+    state = ctypes.cast(state_ptr, ctypes.POINTER(AppStateVars)).contents        
+    data_entry = data_entry_ptr.contents    
+
+    if stream_idx == XRAN_CODELET_OUT_SIDX:
+
+        data_ptr = ctypes.cast(
+            data_entry.data, ctypes.POINTER(struct__packet_stats)
+        )
+
+        data = data_ptr.contents
+        ul_data_stats = data.ul_packet_stats.data_packet_stats
+        dl_data_stats = data.dl_packet_stats.data_packet_stats
+        dl_control_stats = data.dl_packet_stats.ctrl_packet_stats
+
+        print("----------------------------")
+        print(f"Hi App 1: timestamp: {data.timestamp}")
+        print(f"DL Ctl: {dl_control_stats.Packet_count} {list(dl_control_stats.packet_inter_arrival_info.hist)}")
+        print(f"DL Data: {dl_data_stats.Packet_count} {dl_data_stats.Prb_count} {list(dl_data_stats.packet_inter_arrival_info.hist)}")
 
 
 # ######################################################
 # # Main function called from JRTC core
 def jrtc_start_app(capsule):
 
-    XRAN_CODELET_OUT_SIDX = 0
+    streams = [
+        # XRAN_CODELET_OUT_SIDX
+        JrtcStreamCfg_t(
+            JrtcStreamIdCfg_t(
+                JRTC_ROUTER_REQ_DEST_ANY, 
+                JRTC_ROUTER_REQ_DEVICE_ID_ANY, 
+                b"xran_packets_deployment://jbpf_agent/xran_packets/reporter", 
+                b"output_map"),
+            True,   # is_rx
+            None    # No AppChannelCfg 
+        )
+    ]    
 
-    ########################################################
-    # configuration
-    ########################################################
-    app_cfg = AppCfg(
-        context="xran_packets",
-        q_size=100, 
-        streams=[
-            # XRAN_CODELET_OUT_SIDX
-            StreamCfg(
-                sid=StreamIdCfg(
-                    destination=JRTC_ROUTER_REQ_DEST_ANY, 
-                    device_id=JRTC_ROUTER_REQ_DEVICE_ID_ANY, 
-                    stream_source="xran_packets_deployment://jbpf_agent/xran_packets/reporter", 
-                    io_map="output_map"),
-                is_rx=True
-            )
-        ], 
-        sleep_timeout_secs=0.25,
-        inactivity_timeout_secs=2.0
+    app_cfg = JrtcAppCfg_t(
+        b"xran_packets",                               # context
+        100,                                           # q_size
+        len(streams),                                  # num_streams
+        (JrtcStreamCfg_t * len(streams))(*streams),    # streams
+        0.25,                                          # sleep_timeout_secs
+        2.0                                            # inactivity_timeout_secs
     )
 
-    ########################################################
-    # message handler
-    ########################################################
-    def app_handler(timeout: bool, stream_idx: int, data_entry: struct_jrtc_router_data_entry, state: AppStateVars) -> None:
+    # Initialize the app
+    state = AppStateVars()
+    state.app = jrtc_app_create(capsule, app_cfg, app_handler, state)
 
-        if timeout:
+    # run the app - This is blocking until the app exists
+    jrtc_app_run(state.app)
 
-            ## timeout processing
-
-            pass
-
-        elif stream_idx == XRAN_CODELET_OUT_SIDX:
-
-            data_ptr = ctypes.cast(
-                data_entry.data, ctypes.POINTER(struct__packet_stats)
-            )
-
-            data = data_ptr.contents
-            ul_data_stats = data.ul_packet_stats.data_packet_stats
-            dl_data_stats = data.dl_packet_stats.data_packet_stats
-            dl_control_stats = data.dl_packet_stats.ctrl_packet_stats
-
-            print("----------------------------")
-            print(f"Hi App 1: timestamp: {data.timestamp}")
-            print(f"DL Ctl: {dl_control_stats.Packet_count} {list(dl_control_stats.packet_inter_arrival_info.hist)}")
-            print(f"DL Data: {dl_data_stats.Packet_count} {dl_data_stats.Prb_count} {list(dl_data_stats.packet_inter_arrival_info.hist)}")
-
-        else:
-            pass
-
-    ########################################################
-    # App execution handler
-    ########################################################
-    state = AppStateVars(wrapper=None)
-    state.wrapper = JrtcPythonApp(capsule, app_cfg, app_handler, state)
-    state.wrapper.run()
+    # clean up app resources
+    jrtc_app_destroy(state.app)
