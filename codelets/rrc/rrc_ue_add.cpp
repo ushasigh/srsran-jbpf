adc749765098b96446a7d364cab179a563f4e7db
Bozidar Radunovic <bozidar@microsoft.com>
2025-03-31 17:35:35 +0000

    Add initial RRC codelets

---
+++ codelets/rrc/rrc_ue_add.cpp
@@ -0,0 +1,74 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include <linux/bpf.h>
+
+#include "jbpf_srsran_contexts.h"
+
+#include "rrc_ue_add.pb.h"
+
+#include "../utils/misc_utils.h"
+
+
+#define SEC(NAME) __attribute__((section(NAME), used))
+
+#include "jbpf_defs.h"
+#include "jbpf_helper.h"
+
+#define MAX_NUM_UE (32)
+
+jbpf_ringbuf_map(rrc_ue_add_output_map, rrc_ue_add, 256);
+
+struct jbpf_load_map_def SEC("maps") output_map_tmp = {
+  .type = JBPF_MAP_TYPE_ARRAY,
+  .key_size = sizeof(int),
+  .value_size = sizeof(rrc_ue_add),
+  .max_entries = 1,
+};
+
+
+//#define DEBUG_PRINT
+
+extern "C" SEC("jbpf_ran_generic")
+uint64_t jbpf_main(void* state)
+{
+    int zero_index=0;
+    struct jbpf_ran_generic_ctx *ctx = (jbpf_ran_generic_ctx *)state;
+    
+    const jbpf_rrc_ctx_info& rrc_ctx = *reinterpret_cast<const jbpf_rrc_ctx_info*>(ctx->data);
+
+    // Ensure the object is within valid bounds
+    if (reinterpret_cast<const uint8_t*>(&rrc_ctx) + sizeof(uint8_t) + sizeof(jbpf_rrc_ctx_info) > reinterpret_cast<const uint8_t*>(ctx->data_end)) {
+        return JBPF_CODELET_FAILURE;  // Out-of-bounds access
+    }
+
+    rrc_ue_add *out = (rrc_ue_add *)jbpf_map_lookup_elem(&output_map_tmp, &zero_index);
+    if (!out)
+        return JBPF_CODELET_FAILURE;
+
+    out->timestamp = jbpf_time_get_ns();
+    out->ue_index = rrc_ctx.ue_index;
+    /*
+    out->c_rnti = ctx->meta_data1 >> 48;
+    out->pci = (ctx->meta_data1 >> 32) & 0xFFFF;
+    out->tac = (ctx->meta_data1 >> 16) & 0xFFFF;
+    out->plmn = ctx->meta_data1 & 0xFFFFFFFF;
+    out->nci = ctx->meta_data2;
+    */
+    out->c_rnti = 0;
+    out->pci = 0;
+    out->tac = 0;
+    out->plmn = 0;
+    out->nci = 0;
+
+    int ret = jbpf_ringbuf_output(&rrc_ue_add_output_map, (void *)out, sizeof(rrc_ue_add));
+    jbpf_map_clear(&output_map_tmp);
+    if (ret < 0) {
+#ifdef DEBUG_PRINT
+        jbpf_printf_debug("rrc_ue_add: Failure: jbpf_ringbuf_output\n");
+#endif
+        return JBPF_CODELET_FAILURE;
+    }
+
+    return JBPF_CODELET_SUCCESS;
+}
