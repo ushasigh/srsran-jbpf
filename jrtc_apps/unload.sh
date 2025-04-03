17d26219fa49eda5fb0c9b19d6f3b972b528c843
Bozidar Radunovic <bozidar@microsoft.com>
2025-04-03 17:59:54 +0000

    Fix final config issues to get containerized version fully working

--- jrtc_apps/unload.sh
+++ jrtc_apps/unload.sh
@@ -3,7 +3,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-source ../set_vars.sh
+SDK_IMAGE_TAG=latest
+CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+source $CURRENT_DIR/../set_vars.sh
 
 Help()
 {
@@ -36,19 +38,32 @@ if [[ -z "$DEPLOYMENT_YAML" || ! -f "$DEPLOYMENT_YAML" ]]; then
     exit 1
 fi
 
-if [[ -z "$JRTC_CTL_BIN" || ! -f "$JRTC_CTL_BIN" ]]; then
-    echo "Error: DEPLOYMENT_YAML is either not set or the file does not exist."
-    Help
-    exit 1
-fi
-
 DEPLOYMENT_YAML_FILENAME=$(basename "$DEPLOYMENT_YAML")
 DEPLOYMENT_YAML_PATH=$(dirname "$DEPLOYMENT_YAML")
 
-pushd . > /dev/null
-cd $DEPLOYMENT_YAML_PATH
-$JRTC_CTL_BIN unload -c $DEPLOYMENT_YAML_FILENAME --log-level trace
-ret=$?
-popd > /dev/null
+if [ "$SRS_JBPF_DOCKER" -eq 1 ]; then
+    $DOCKER_CMD run --network=host \
+        -v $CURRENT_DIR:/apps \
+        -v $CURRENT_DIR/../codelets:/codelets \
+        -e "JRTC_APPS=/apps" \
+        -e "JBPF_CODELETS=/codelets" \
+        -e "JRTC_PATH=/jrtc" \
+        --entrypoint /jrtc/bin/jrtc-ctl \
+        ghcr.io/microsoft/jrt-controller/jrt-controller-azurelinux:$SDK_IMAGE_TAG \
+        unload -c /apps/$DEPLOYMENT_YAML --log-level trace
+    ret=$?
+else
+    if [[ -z "$JRTC_CTL_BIN" || ! -f "$JRTC_CTL_BIN" ]]; then
+        echo "Error: JRTC_CTL_BIN is either not set or the file does not exist."
+        Help
+        exit 1
+    fi
+
+    pushd . > /dev/null
+    cd $DEPLOYMENT_YAML_PATH
+    $JRTC_CTL_BIN unload -c $DEPLOYMENT_YAML_FILENAME --log-level trace
+    ret=$?
+    popd > /dev/null
+fi
 
 exit $ret
